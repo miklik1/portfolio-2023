7802bb94dd2ac720fba8331bf87d278cd8862937
Martin Miklik <martin.miklik@brandmaster.com>
2023-10-29 22:46:58 +0100

    navbar fix

--- src/components/navigation/Navigation.component.tsx
+++ src/components/navigation/Navigation.component.tsx
@@ -38,7 +38,7 @@ export default function Example() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <div className="hidden sm:ml-6 sm:block">
-                  
+
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <Link to={item.name} smooth={true} duration={500}
@@ -61,21 +61,22 @@ export default function Example() {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-blue' : 'text-gray-300 hover:bg-gray-700 hover:text-red',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
+                <Link key={item.name} to={item.name} smooth={true} duration={500}>
+                  <Disclosure.Button
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-blue' : 'text-gray-300 hover:bg-gray-700 hover:text-red',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                </Link>
               ))}
             </div>
-            </Disclosure.Panel>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
